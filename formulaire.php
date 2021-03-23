4436175bb29faa86f7a5f7c75ab217805cd194cc
ColsonB <bcolson@la-providence.net>
2021-03-23 16:45:17 +0100

    Commit

--- formulaire.php
+++ formulaire.php
@@ -15,6 +15,7 @@ if (session_status() == PHP_SESSION_NONE) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="src/css/style.css">
         <title>Connexion</title>
     </head>
 
