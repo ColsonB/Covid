5c11f005fd3ba88788dd8dcd293606119411436e
ColsonB <bcolson@la-providence.net>
2021-03-23 16:40:16 +0100

    Commit

--- fonction.php
+++ fonction.php
@@ -1,7 +1,9 @@
 <?php
-try{
-    $BDD=new PDO('mysql:host=mysql-bcolson.alwaysdata.net; dbname=bcolson_virus; charset=utf8','bcolson_site','Colson1234*');
-}catch(Exception $e){
-    die('Erreur : ' . $e->getMessage());
-}
+
+    try{
+        $BDD=new PDO('mysql:host=mysql-bcolson.alwaysdata.net; dbname=bcolson_virus; charset=utf8','bcolson_site','Colson1234*');
+    }catch(Exception $e){
+        die('Erreur : ' . $e->getMessage());
+    }
+    
 ?>
--- inscription.php
+++ inscription.php
@@ -27,9 +27,6 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Inscription</title>
-        <link rel="icon" type="image/png" href="src/img/icone.png">
-        <link rel='stylesheet' type='text/css' href='src/css/page.css'>
-        <link rel='stylesheet' type='text/css' href='src/css/inscription.css'>
     </head>
 
     <body>
