711a26a63a00ddaf54f8704722fd92b99f518966
ColsonB <bcolson@la-providence.net>
2021-03-23 17:28:02 +0100

    Commit

---
+++ Correction/index.php
@@ -0,0 +1,31 @@
+<?php 
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+    <?
+    include "fonction.php"; 
+
+    if($access){
+        
+        echo "BIENVENUE sur MON SITE";
+        echo '<a href="combat.php">vient combatre</a>';
+
+    }else{
+        echo "c'est ouf";
+        echo $errorMessage;
+    }
+    ?>
+</body>
+</html>
--- fonction.php
+++ fonction.php
@@ -1,9 +1,9 @@
 <?php
 
     try{
-        $BDD=new PDO('mysql:host=mysql-bcolson.alwaysdata.net; dbname=bcolson_virus; charset=utf8','bcolson_site','Colson1234*');
+        $BDD=new PDO('mysql:host=mysql-bcolson.alwaysdata.net; dbname=bcolson_virus; charset=utf8','bcolson_site','TDataSource1234');
     }catch(Exception $e){
         die('Erreur : ' . $e->getMessage());
     }
-    
+
 ?>
